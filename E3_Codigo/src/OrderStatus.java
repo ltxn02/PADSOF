faf8dc3091a8638efd690643b03179979ef032f5
DrSenkuuStone <taha.ridda@estudiante.uam.es>
2026-03-11 19:01:39 +0100

    Add files via upload

---
+++ E3_Codigo/src/OrderStatus.java
@@ -0,0 +1,11 @@
+
+public enum OrderStatus {
+	SIN_PAGAR,
+	EN_PREPARACIÓN,
+	ENTREGADO;
+	
+	public String toString() {
+		return this.name();
+	}
+	
+}
---
+++ E3_Codigo/src/SecondHandProduct.java
@@ -0,0 +1,29 @@
+import java.util.List;
+/**
+ * Clase para representar los productos de segunda mano
+ * @author Taha Ridda
+ * @version 1.0
+ *
+ */
+public class SecondHandProduct {
+	private int secondHandId;
+	private boolean isAppraised;
+	private boolean isOffered;
+	//private Client Owner;
+	private ItemType Itemtype;
+	private Condition condition;
+	
+	public SecondHandProduct(int secondHandId, boolean isAppraised, boolean isOffered, ItemType ItemType, Condition condition, /**Client Owner*/) {
+
+		this.secondHandId = secondHandId;
+		this.isAppraised = isAppraised;
+		this.isOffered = isOffered;
+		this.Itemtype = ItemType;
+		this.condition = condition;
+		//this.Owner = Owner;
+
+	}
+	
+	
+	
+}
