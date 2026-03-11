4c72bc649d40611de00c2f0e2b770969b8a0ae1d
DrSenkuuStone <taha.ridda@estudiante.uam.es>
2026-03-11 19:27:42 +0100

    Add files via upload

--- E3_Codigo/src/Condition.java
+++ E3_Codigo/src/Condition.java
@@ -1,3 +1,9 @@
+/**
+ * Enum para representar la condición de los productos de segunda mano para intercambios
+ * @author Taha Ridda
+ * @version 1.0
+ *
+ */
 public enum Condition {
 	PERFECTO,
 	MUY_BUENO,
--- E3_Codigo/src/OrderStatus.java
+++ E3_Codigo/src/OrderStatus.java
@@ -1,4 +1,9 @@
-
+/**
+ * Enum para representar el estado del pedido
+ * @author Taha Ridda
+ * @version 1.0
+ *
+ */
 public enum OrderStatus {
 	SIN_PAGAR,
 	EN_PREPARACIÓN,
