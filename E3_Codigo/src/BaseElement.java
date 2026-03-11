430ec8a308d0e4db10d70455c7d0007b996fe5fc
ltxn02 <lidia.mteres@gmail.com>
2026-03-11 19:09:11 +0100

    Creada clase BaseElement y añadidos archivos User y RegisteredUser

---
+++ E3_Codigo/src/BaseElement.java
@@ -0,0 +1,31 @@
+import java.time.Instant;
+
+public abstract class BaseElement {
+	private boolean active;
+	private Instant createdAt;
+	private Instant updatedAt;
+	
+	public BaseElement() {
+		this.active = true;
+		this.createdAt = Instant.now();
+		this.updatedAt = Instant.now();
+	}
+	
+	public void markAs(boolean active/*, RegisteredUser by*/) {
+		this.active = active;
+		this.updatedAt = Instant.now();
+	}
+	
+	@Override
+	public String toString() {
+		String str = "Status: ";
+		
+		if (this.active == true) {
+			str += "active. ";
+		} else {
+			str += "inactive. ";
+		}
+
+		return str + "Created at: " + this.createdAt + ". Last updated: " + this.updatedAt;
+	}
+}
---
+++ E3_Codigo/src/RegisteredUser.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public abstract class RegisteredUser extends User {
+	private static int lastUserId;
+	private int userId;
+	private String username;
+	private String password;
+	private String fullname;
+	private String dni;
+	private Date birthdate;
+	private String email;
+	private String phoneNumber;
+	private List<Notification> myNotifications;
+	
+	public RegisteredUser(String username, String password, String fullname, String dni, Date birthdate, String email, String phoneNumber) {
+		this.userId = next_id();
+		this.username = username;
+		this.password = password;
+		this.fullname = fullname;
+		this.dni = dni;
+		this.birthdate = birthdate;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	private int next_id() {
+		int nextId = lastUserId;
+		lastUserId++;
+		return nextId;
+	}
+	
+	public boolean login(String username, String password) {
+		if (username == this.username && password == this.password) {
+			return true;
+		}
+		return false;
+	}
+	
+	// public 
+	
+	@Override
+	public String toString() {
+		
+	}
+}
