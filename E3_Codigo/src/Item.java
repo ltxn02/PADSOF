11fec8aac83953d74f33f4a6bbc54304b775da6c
ivansuitoo <ivansuitoo>
2026-03-11 19:13:03 +0100

    clase item

---
+++ E3_Codigo/src/Item.java
@@ -0,0 +1,19 @@
+public class Item{
+    private String name;
+    private String description;
+    private double price;
+    private String picturePath;
+    
+    public Item(String name, String description, double price, String picturePath){
+        this.name = name;
+        this.description = description;
+        
+        /* si el precio introducido es menor a 0 se pone el valor por defecto 1 */
+        if (price < 0){
+            this.price = 1;
+        } else {
+            this.price = price;
+        }
+        this.picturePath = picturePath;
+    }
+}
