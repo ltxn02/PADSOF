6f0234349447a74d807f0e0093e3b6e752e993a3
ivansuitoo <ivansuitoo>
2026-03-25 19:01:04 +0100

    Fixeado errores

--- E3_Codigo/src/Comic.java
+++ E3_Codigo/src/Comic.java
@@ -6,7 +6,8 @@ public class Comic extends Product{
     private int publicationYear;
     private ArrayList<String> writtenBy;
 
-    public Comic(int nPages, String publisher, int publicationYear, ArrayList<String> writtenBy) {
+    public Comic(String name, String description, double price, String image, ArrayList<Category> categories, int stock, ArrayList<Review> reviews, Discount discount, int nPages, String publisher, int publicationYear, ArrayList<String> writtenBy) {
+        super(name, description, price, image, categories, stock, reviews, discount);
         if (nPages <= 0) {
             this.nPages = 1;
         } else {
--- E3_Codigo/src/Figurine.java
+++ E3_Codigo/src/Figurine.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Figurine extends Product{
     private double height;
     private double width;
@@ -5,7 +7,8 @@ public class Figurine extends Product{
     private String material;
     private String franchise;
 
-    public Figurine(double height, double width, double depth, String material, String franchise) {
+    public Figurine(String name, String description, double price, String image, ArrayList<Category> categories, int stock, ArrayList<Review> reviews, Discount discount, double height, double width, double depth, String material, String franchise) {
+        super(name, description, price, image, categories, stock, reviews, discount);
         if (height < 0 || width < 0 || depth < 0) {
             throw new IllegalArgumentException("Argumentos invalidos");
         }
