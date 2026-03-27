c95450c33b132ce67b1038b8556df07329fe48b7
ltxn02 <lidia.mteres@gmail.com>
2026-03-27 18:32:29 +0100

    Cambiado tipo de dato

--- E3_Codigo/src/Employee.java
+++ E3_Codigo/src/Employee.java
@@ -6,7 +6,7 @@ public class Employee extends Staff{
     private boolean enabled;
     public ArrayList<EmployeeRoles> Rol;
     public ArrayList<Permission> permissions;
-    public Employee(String username, String password, String fullname, String dni, Date birthdate, String email, String phoneNumber, double salary, boolean enabled){
+    public Employee(String username, String password, String fullname, String dni, String birthdate, String email, String phoneNumber, double salary, boolean enabled){
         super(username, password, fullname, dni, birthdate, email, phoneNumber, salary);
         this.enabled = enabled;
     }
--- E3_Codigo/src/Manager.java
+++ E3_Codigo/src/Manager.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 public class Manager extends Staff{
 
-    public Manager (String username, String password, String fullname, String dni, Date birthdate, String email, String phoneNumber, double salary){
+    public Manager (String username, String password, String fullname, String dni, String birthdate, String email, String phoneNumber, double salary){
         super(username, password, fullname, dni, birthdate, email, phoneNumber, salary);
 
     }
