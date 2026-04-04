ac65ee652e12cb9f8f848460d066000e38678242
Taha Ridda <taha.ridda@estudiante.uam.es>
2026-04-04 23:53:02 +0200

    prueba

---
+++ E3_Codigo/src/rongero/model/users/Manager.java
@@ -0,0 +1,11 @@
+package rongero.model.users;
+import java.util.Date;
+
+public class Manager extends Staff {
+
+    public Manager (String username, String password, String fullname, String dni, String birthdate, String email, String phoneNumber, double salary){
+        super(username, password, fullname, dni, birthdate, email, phoneNumber, salary);
+
+    }
+
+}
