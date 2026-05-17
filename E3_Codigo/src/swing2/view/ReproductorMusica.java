1c9f8538ad49e8d05bee33cd321074d037ed8923
iTaha-R <taha.ridda@estudiante.uam.es>
2026-05-17 23:53:40 +0200

    actualizacion

--- E3_Codigo/src/swing2/view/ReproductorMusica.java
+++ E3_Codigo/src/swing2/view/ReproductorMusica.java
@@ -1,40 +1,47 @@
 package swing2.view;
+
 import javax.sound.sampled.*;
 import java.net.URL;
 
 public class ReproductorMusica {
-    private Clip clip;
 
     public void reproducirMusica(String ruta) {
         try {
-            // Buscamos el archivo (debe ser .wav para máxima compatibilidad)
+            
             URL url = getClass().getResource(ruta);
+
+            
             if (url == null) {
-                url = getClass().getResource("../.." + ruta);
+                url = getClass().getResource("/foto/" + ruta);
             }
 
             if (url != null) {
-                AudioInputStream audioInput = AudioSystem.getAudioInputStream(url);
-                clip = AudioSystem.getClip();
-                clip.open(audioInput);
+                AudioInputStream ais = AudioSystem.getAudioInputStream(url);
+                AudioFormat baseFormat = ais.getFormat();
 
-                // Configuramos el bucle infinito
+                
+                AudioFormat targetFormat = new AudioFormat(
+                        AudioFormat.Encoding.PCM_SIGNED,
+                        44100,
+                        16,
+                        baseFormat.getChannels(),
+                        baseFormat.getChannels() * 2,
+                        44100,
+                        false
+                );
+
+                AudioInputStream decodedAis = AudioSystem.getAudioInputStream(targetFormat, ais);
+                Clip clip = AudioSystem.getClip();
+                clip.open(decodedAis);
                 clip.loop(Clip.LOOP_CONTINUOUSLY);
-
-                // Ajustar el volumen (opcional, entre 0.0 y 1.0)
-                FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-                gainControl.setValue(-10.0f); // Baja un poco el volumen (en decibelios)
-
                 clip.start();
+            } else {
+                System.err.println("No se encontró el archivo de audio en: " + ruta);
             }
+
         } catch (Exception e) {
             System.err.println("Error al reproducir música: " + e.getMessage());
-        }
-    }
-
-    public void detenerMusica() {
-        if (clip != null && clip.isRunning()) {
-            clip.stop();
+            e.printStackTrace();
         }
     }
 }
